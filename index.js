d228b4410a8166154aa1ac79678ea4a32029fdd7
Vegard Bakke <Vegard.Bakke@bwise.no>
2021-04-30 09:24:06 +0200

    First draft of argon-xor encryption

--- index.js
+++ index.js
@@ -5,19 +5,27 @@ const vault = require('./src/iota-seen-encrypt/iota-seed-encrypt');
 
     let seed = new Uint8Array(vault.SEED_LEN);
     let salt = new Uint8Array(vault.SALT_LEN);
-    encrypted = await vault.encryptSeed(seed, 'ThisIsABenchmark!123', null, '13245678');
-    encrypted = await vault.encryptSeed(seed, 'password', null, salt);
-    console.log(encrypted, encrypted.length);
-    console.log(encrypted = await vault.encryptSeed(seed, 'password', 1, 'saltsalt'));
-    console.log(encrypted = await vault.encryptSeed(seed, 'password', 1, salt));
-
-    salt[0]+= 0x80;
-    salt[vault.SALT_LEN-1]++;
-
-    encrypted = await vault.encryptSeed(seed, 'pass', null, salt);
-    console.log(encrypted);
+    let encrypted = await vault.encryptSeed(seed, 'argon2', 1, salt);
+    console.log(encrypted.length, encrypted);
+    let decrypted = await vault.decryptSeed(encrypted, 'argon2');
+    console.log(decrypted.length, decrypted);
     
-    encrypted = await vault.generateEncryptedSeed('Pãssprasë');
-    console.log(encrypted);
+    // let key;
+    // for (let t=0; t<10; t++) {
+    //      await vault.generateKey('argon2', '12345678', t-5);
+    // }
+    // encrypted = await vault.encryptSeed(seed, 'ThisIsABenchmark!123', null, '12345678');
+    // console.log(encrypted, encrypted.length);
+    // console.log(encrypted = await vault.encryptSeed(seed, 'password', 1, 'saltsalt'));
+    // console.log(encrypted = await vault.encryptSeed(seed, 'password', 1, salt));
+
+    // salt[0]+= 0x80;
+    // salt[vault.SALT_LEN-1]++;
+
+    // encrypted = await vault.encryptSeed(seed, 'pass', null, salt);
+    // console.log(encrypted);
+    
+    // encrypted = await vault.generateEncryptedSeed('Pãssprasë');
+    // console.log(encrypted);
 
 })();
--- src/iota-seen-encrypt/iota-seed-encrypt.js
+++ src/iota-seen-encrypt/iota-seed-encrypt.js
@@ -2,9 +2,10 @@
  * 
  */
 const crypto = require('crypto');
+const xor = require('buffer-xor');
 const bip39 = require('bip39');
-const { argon2id, argon2Verify } = require('hash-wasm');
 const aes = require('aes-js');
+const { argon2id, argon2Verify } = require('hash-wasm');
 const base32 = require('base32.js');
 
 
@@ -12,8 +13,9 @@ const base32 = require('base32.js');
 
 
 const SEED_LEN = 32;
-const SALT_LEN = 12;
-const AES_CTR_LEN = 16;
+const SALT_LEN = 8;
+const CHECK_LEN = 4;
+const VERSION = 0x01;
 
 
 async function generateEncryptedSeed(passphrase, toughness, salt) {
@@ -23,32 +25,70 @@ async function generateEncryptedSeed(passphrase, toughness, salt) {
 
 async function encryptSeed(seed, passphrase, toughness, salt) {
     toughness = toughness || 0;
-    salt = salt || crypto.randomBytes(SALT_LEN);
-
+    salt =  generateSalt(SALT_LEN);
+    seed[0] = 0xF0;
+    let len = seed.length;
     let time = Date.now();
-    const key = await generateKey(passphrase, salt, toughness);
+    const key64 = await generateKey(passphrase, salt, 2*len, toughness);
     time = Date.now() - time;
-    console.log('Secret key: '+key.toString('hex'))
-    const encrypted = await encrypt(seed, key, salt);
+    let key1 = key64.slice(0, len);
+    let key2 = key64.slice(len);
 
-    let merged = merge(encrypted, salt);
+    let encrypted = xor(seed, key1);
+
+
+    let checksum = await generateChecksum(key2, encrypted, CHECK_LEN);
+    let header = new Uint8Array([VERSION]);
+    
+
+    let merged = merge(header, encrypted, salt, checksum);
+    console.log('DEBUG: salt: ', salt);
+    console.log('DEBUG: checksum: ', checksum);
 
     let encoded = base32.encode(merged);
-    //if (toughness != 0)
-    encoded += ':T'+toughness;
+    if (toughness != 0) encoded += ':T'+toughness;
+    
     return encoded;
+}
+
+async function decryptSeed(encrypted, passphrase) {
+    let info = extractInfo(encrypted);
+    let salt =  info.salt;
+    let toughness =  info.toughness;
+    let version =  info.version;
+
+    if (version !== VERSION) {
+        throw Error('Encrypted seed is unknown version.');
+    }
+
+    let len = SEED_LEN;
+    let time = Date.now();
+    const key64 = await generateKey(passphrase, salt, 2*len, toughness);
+    time = Date.now() - time;
+    let key1 = key64.slice(0, len);
+    let key2 = key64.slice(len);
+
+    let seed = xor(info.encrypted, key1);
+    let checksum = await generateChecksum(key2, info.encrypted, CHECK_LEN);
+
+    if (!equalBytes(checksum,  info.checksum)) {
+        throw Error('Inncorrect passphrase');
+    }
+
+    
+    return seed;
 }
 
 
 
-const preEncoding = {
+const tempEncoding = {
     'bip39': 'hex',
     'binary': null,
 }
 
 function generateSeed(encoding) {
     encoding = encoding || 'HEX';
-    let first_encoding = (preEncoding.hasOwnProperty(encoding)) ? preEncoding[encoding] : encoding;
+    let first_encoding = (tempEncoding.hasOwnProperty(encoding)) ? tempEncoding[encoding] : encoding;
     let seed = crypto.randomBytes(SEED_LEN);
     
     if (first_encoding)
@@ -61,31 +101,68 @@ function generateSeed(encoding) {
 }
 
 
-async function generateKey(passphrase, salt, toughness) {
-    let opts = argonOptions(toughness);
-    opts.salt = salt;
-    opts.password = aes.utils.utf8.toBytes(passphrase);
-    opts.hashLength = 32; // bytes
-    opts.outputType = 'binary'; // return standard encoded string containing parameters needed to verify the key
+async function generateKey(passphrase, salt, len, toughness) {
+    let opts = argonOptions(passphrase, salt, len, toughness);
 
     let start = Date.now();
     let encryptionKey = await argon2id(opts);
     let time = Date.now() - start;
-    //console.log('Generating key in '+time+' ms');
+    //console.log('Generating key in '+time+' ms: '+Buffer.from(encryptionKey).toString('base64'));
+    console.log('T: '+toughness+' Mem: '+(Math.log(opts.memorySize)/Math.log(2)).toFixed(1)+' iter: '+opts.iterations+': '+time+' ms: '+Buffer.from(encryptionKey).toString('hex'));
     return encryptionKey;
 }
 
+function generateSalt(len) {
+    let salt = crypto.randomBytes(len);
+    
+    // for (let i = 0; i<len; i++) {
+    //     salt[i] = i.toString();
+    // }
 
-function encrypt(plain, encryptionKey, counter) {
-    if (counter instanceof Uint8Array && counter.length < AES_CTR_LEN) {
-        counter = extendArray(counter, AES_CTR_LEN);
-    }
-
-    const cryptor = new aes.ModeOfOperation.ctr(encryptionKey, counter);
-    const encrypted = cryptor.encrypt(plain);
-    return encrypted;
+    return salt;
 }
 
+async function generateChecksum(key, encrypted, len) {
+    let opts = {
+        memorySize: 8, //kilobytes
+        parallelism: 1,
+        iterations: 1,
+        hashLength: len,
+        password: key,
+        salt: encrypted,
+        outputType: 'binary', 
+    }
+
+    let checksum = await argon2id(opts);
+
+    return checksum;
+}
+
+function packInfo(info) {
+    return info;
+}
+function extractInfo(qrstr) {
+    let split = qrstr.split(':');
+    let toughness = 0;
+    if (split.length>1) {
+        let tail = split[1];
+        if (tail[0] === 'T') {
+            try {
+                toughness = parseInt(tail.slice(1));
+            } catch  {
+                // pass
+            }
+        }
+    }
+    let bytes = base32.decode(split[0]);
+
+    let version = bytes.slice(0, 1)[0];
+    let encrypted = bytes.slice(1, 1+SEED_LEN);
+    let salt = bytes.slice(1+SEED_LEN, 1+SEED_LEN+SALT_LEN);
+    let checksum = bytes.slice(1+SEED_LEN+SALT_LEN);
+
+    return {version, salt, encrypted, checksum, toughness};
+}
 
 function seedToMnemonic(seed) {
     const mnemonic = bip39.entropyToMnemonic(seed);
@@ -102,22 +179,52 @@ function extendArray(arr, len) {
     const zero = new Uint8Array(len - arr.length);
     return merge(zero, arr);
 }
-function merge(arr1, arr2) {
-    const merged = new Uint8Array(arr1.length + arr2.length);
-    merged.set(arr1);
-    merged.set(arr2, arr1.length);
+function merge(...args) {
+    let len = 0;
+    for (let arr of args) len += arr.length;
+
+    const merged = new Uint8Array(len);
+    let pos = 0;
+    for (let arr of args) {
+        merged.set(arr, pos);
+        pos += arr.length;
+    }
+    
     return merged;
 }
-
-function argonOptions(toughness) {
-    let iter_exp = 10 + toughness;
-    let iterations = Math.pow(2, iter_exp);
-    //console.log('Argon toughness: '+toughness+' iterations: '+iterations);
-    return {
-        parallelism: 1,
-        iterations,
-        memorySize: 512, // use 512KB memory
+function equalBytes (buf1, buf2)
+{
+    if (buf1.byteLength != buf2.byteLength) return false;
+    // var dv1 = new Int8Array(buf1);
+    // var dv2 = new Int8Array(buf2);
+    for (var i = 0 ; i != buf1.byteLength ; i++)
+    {
+        if (buf1[i] !== buf2[i]) return false;
     }
+    return true;
+}
+
+function argonOptions(passphrase, salt, hashLength, toughness) {
+    hashLength = hashLength || 32;
+    toughness = toughness || 0;
+    let sq2 = Math.sqrt(2);
+    let memory =  64*1024 * Math.pow(sq2, toughness);
+    let iterations = 8 * Math.pow(sq2, toughness);
+    if (typeof passphrase === 'string')  passphrase =  passphrase.normalize();
+    if (typeof salt === 'string')  salt =  salt.normalize();
+
+    let opts = {
+        memorySize: Math.floor(memory), //kilobytes
+        parallelism: 4,
+        iterations: Math.floor(iterations),
+        hashLength: hashLength,
+        password: passphrase,
+        salt: salt,
+        outputType: 'binary', 
+    }
+
+    //console.log('Argon toughness: '+toughness+' iterations: '+iterations);
+    return opts; 
 }
 
 
@@ -127,5 +234,6 @@ module.exports = {
     generateSeed,
     generateEncryptedSeed,
     encryptSeed,
-
+    decryptSeed,
+    generateKey,
 }
